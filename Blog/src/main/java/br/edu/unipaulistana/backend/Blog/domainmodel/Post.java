1043215488254a66c737cfa538904aa7a5dbd978
lab05l <matheus.martins07@outlook.com>
2025-09-03 20:28:29 -0300

    teste

---
+++ Blog/src/main/java/br/edu/unipaulistana/backend/Blog/domainmodel/Post.java
@@ -0,0 +1,10 @@
+package br.edu.unipaulistana.backend.Blog.domainmodel;
+
+public class Post {
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+    private String role;
+    private Profile profile;
+}
